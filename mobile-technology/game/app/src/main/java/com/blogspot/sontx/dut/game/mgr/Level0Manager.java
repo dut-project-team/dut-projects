e604cc97e62b6d97be4d4a83f60b19101455374e
xuan son tran <xuanson33bk@gmail.com>
2016-01-19 14:47:59 +0700

    + hole can auto horizontal/vertical move

--- mobile-technology/game/app/src/main/java/com/blogspot/sontx/dut/game/mgr/Level0Manager.java
+++ mobile-technology/game/app/src/main/java/com/blogspot/sontx/dut/game/mgr/Level0Manager.java
@@ -1,6 +1,7 @@
 package com.blogspot.sontx.dut.game.mgr;
 
 import android.graphics.Color;
+import android.graphics.RectF;
 
 import com.blogspot.sontx.dut.game.obj.Hole;
 
@@ -20,8 +21,10 @@ public class Level0Manager extends PlayingManager {
         generateBall();
 
         Hole hole = new Hole(DRAWABLE_X + DRAWABLE_WIDTH / 2.0f - HOLE_RADIUS, DRAWABLE_Y, Color.BLACK);
+        hole.setMovableBound(new RectF(DRAWABLE_X, DRAWABLE_Y, DRAWABLE_X + DRAWABLE_WIDTH, DRAWABLE_Y + DRAWABLE_HEIGHT));
         hole.setWidth(HOLE_RADIUS * 2.0f);
         hole.setHeight(HOLE_HEIGHT);
+        hole.setSpeedX(5.5f);
         addHole(hole);
     }
 }
--- mobile-technology/game/app/src/main/java/com/blogspot/sontx/dut/game/obj/Hole.java
+++ mobile-technology/game/app/src/main/java/com/blogspot/sontx/dut/game/obj/Hole.java
@@ -6,8 +6,29 @@ import android.graphics.Canvas;
  * Created by Noem on 17/1/2016.
  */
 public class Hole extends MovableObject {
+    @Override
+    public void setSpeedX(float speed) {
+        super.setSpeedX(speed);
+        mSpeedY = 0.0f;
+    }
+
+    @Override
+    public void setSpeedY(float speed) {
+        super.setSpeedY(speed);
+        mSpeedX = 0.0f;
+    }
+
     public Hole(float x, float y, int color) {
         super(x, y, color);
+    }
+
+    @Override
+    protected void update0() {
+        super.update0();
+        if (mRect.left < mMovableBound.left || mRect.right > mMovableBound.right)
+            mSpeedX = -mSpeedX;
+        if (mRect.top < mMovableBound.top || mRect.bottom > mMovableBound.bottom)
+            mSpeedY = -mSpeedY;
     }
 
     @Override
