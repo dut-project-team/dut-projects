11243aa6e14257a2a8a29a0635c29f4d76d9e7b2
xuanson33bk <xuanson33bk@gmail.com>
2016-03-23 21:04:45 +0700

    - using appcompat instead of normal activity for more beautiful

--- mobile-technology/game/app/src/main/java/com/blogspot/sontx/dut/game/ui/MainActivity.java
+++ mobile-technology/game/app/src/main/java/com/blogspot/sontx/dut/game/ui/MainActivity.java
@@ -2,6 +2,7 @@ package com.blogspot.sontx.dut.game.ui;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.blogspot.sontx.dut.game.lib.SystemAlert;
 
@@ -9,7 +10,7 @@ import com.blogspot.sontx.dut.game.lib.SystemAlert;
  * Copyright by SONTX 2016. www.sontx.in
  * Created by Noem on 16/1/2016.
  */
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
