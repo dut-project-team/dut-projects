856e27f78c4412660f8d02fd9a9959f7e2620c0e
xuan son tran <xuanson33bk@gmail.com>
2016-04-09 16:17:19 +0700

    + add ButtonPanel support group radio buttons

---
+++ soft-technology/PrimarySchool/src/com/blogspot/sontx/dut/primaryschool/ui/ButtonPanel.java
@@ -0,0 +1,19 @@
+package com.blogspot.sontx.dut.primaryschool.ui;
+
+import java.awt.Component;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+
+public class ButtonPanel extends JPanel {
+	private static final long serialVersionUID = 1L;
+	private ButtonGroup ButtonGroup = new ButtonGroup();
+
+	@Override
+	public Component add(Component comp) {
+		if (comp instanceof JRadioButton)
+			ButtonGroup.add((JRadioButton) comp);
+		return super.add(comp);
+	}
+}
