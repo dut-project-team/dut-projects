43563009a6fd12abf1aa5f8f466d74344e03afd8
KofD <khanhofd@gmail.com>
2016-04-22 13:59:49 +0700

    + final version

--- soft-technology/PrimarySchool/src/com/blogspot/sontx/dut/primaryschool/ui/CapNhatGiaoVien.java
+++ soft-technology/PrimarySchool/src/com/blogspot/sontx/dut/primaryschool/ui/CapNhatGiaoVien.java
@@ -1,11 +1,14 @@
 package com.blogspot.sontx.dut.primaryschool.ui;
 import javax.swing.JTextField;
+
 import com.blogspot.sontx.dut.primaryschool.ui.date.JCalendar;
 import com.blogspot.sontx.dut.primaryschool.ui.date.JDateChooser;
+
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JRadioButtonMenuItem;
 import javax.swing.ButtonGroup;
+import javax.swing.JRadioButton;
 //KofD
 public class CapNhatGiaoVien extends Window{
 	private JTextField tFHoVaTen;
@@ -17,59 +20,80 @@ public class CapNhatGiaoVien extends Window{
 		setTitle("C\u1EADp Nh\u1EADt Gi\u00E1o Vi\u00EAn");
 		
 		tFHoVaTen = new JTextField();
-		tFHoVaTen.setBounds(382, 52, 167, 20);
+		tFHoVaTen.setBounds(382, 50, 167, 20);
 		getContentPane().add(tFHoVaTen);
 		tFHoVaTen.setColumns(10);
 		
 		JDateChooser dCNgaySinh = new JDateChooser();
-		dCNgaySinh.setBounds(382, 83, 111, 20);
+		dCNgaySinh.setBounds(382, 80, 111, 20);
 		getContentPane().add(dCNgaySinh);
 		
 		tFDiaChi = new JTextField();
 		tFDiaChi.setColumns(10);
-		tFDiaChi.setBounds(382, 117, 167, 20);
+		tFDiaChi.setBounds(382, 110, 167, 20);
 		getContentPane().add(tFDiaChi);
 		
 		tFQueQuan = new JTextField();
 		tFQueQuan.setColumns(10);
-		tFQueQuan.setBounds(382, 154, 167, 20);
+		tFQueQuan.setBounds(382, 140, 167, 20);
 		getContentPane().add(tFQueQuan);
 		
 		tFSoDienThoai = new JTextField();
 		tFSoDienThoai.setColumns(10);
-		tFSoDienThoai.setBounds(382, 198, 167, 20);
+		tFSoDienThoai.setBounds(382, 170, 167, 20);
 		getContentPane().add(tFSoDienThoai);
 		
 		JButton btnCapNhat = new JButton("C\u1EADp Nh\u1EADt");
-		btnCapNhat.setBounds(295, 282, 89, 23);
+		btnCapNhat.setBounds(295, 250, 89, 23);
 		getContentPane().add(btnCapNhat);
 		
 		btnHuy = new JButton("H\u1EE7y");
-		btnHuy.setBounds(421, 282, 89, 23);
+		btnHuy.setBounds(421, 250, 89, 23);
 		getContentPane().add(btnHuy);
 		
 		JLabel lblHoVaTen = new JLabel("H\u1ECD V\u00E0 T\u00EAn");
-		lblHoVaTen.setBounds(201, 55, 111, 14);
+		lblHoVaTen.setBounds(201, 50, 111, 14);
 		getContentPane().add(lblHoVaTen);
 		
 		JLabel lblNgaySinh = new JLabel("Ng\u00E0y Sinh");
-		lblNgaySinh.setBounds(201, 89, 111, 14);
+		lblNgaySinh.setBounds(201, 80, 111, 14);
 		getContentPane().add(lblNgaySinh);
 		
 		JLabel lblDiaChi = new JLabel("\u0110\u1ECBa Ch\u1EC9");
-		lblDiaChi.setBounds(201, 120, 111, 14);
+		lblDiaChi.setBounds(201, 110, 111, 14);
 		getContentPane().add(lblDiaChi);
 		
 		JLabel lblQueQuan = new JLabel("Qu\u00EA Qu\u00E1n");
-		lblQueQuan.setBounds(201, 157, 111, 14);
+		lblQueQuan.setBounds(201, 140, 111, 14);
 		getContentPane().add(lblQueQuan);
 		
 		JLabel lblSoDienThoai = new JLabel("S\u1ED1 \u0110i\u1EC7n Tho\u1EA1i");
-		lblSoDienThoai.setBounds(201, 201, 111, 14);
+		lblSoDienThoai.setBounds(201, 170, 111, 14);
 		getContentPane().add(lblSoDienThoai);
 		
 		JLabel lblGioiTinh = new JLabel("Gi\u1EDBi T\u00EDnh");
-		lblGioiTinh.setBounds(201, 242, 111, 14);
+		lblGioiTinh.setBounds(201, 200, 111, 14);
 		getContentPane().add(lblGioiTinh);
+		
+		ButtonPanel buttonPanel = new ButtonPanel();
+		buttonPanel.setBounds(382, 200, 177, 23);
+		getContentPane().add(buttonPanel);
+		
+		JRadioButton rdbtnNam = new JRadioButton("Nam");
+		rdbtnNam.setBounds(6, 0, 52, 23);
+		buttonPanel.add(rdbtnNam);
+		
+		JRadioButton rdbtnNu = new JRadioButton("N\u1EEF");
+		rdbtnNu.setBounds(86, 0, 52, 23);
+		buttonPanel.add(rdbtnNu);
+	}
+	@Override
+	protected void onClicked(String whoClicked) {
+		// TODO Auto-generated method stub
+		
+	}
+	public static void main(String[] args){
+		CapNhatGiaoVien kofd = new CapNhatGiaoVien();
+		kofd.setVisible(true);
 	}
 }
